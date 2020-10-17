a0d5fd5b879d4c79ca20ae6db41f00687905d1e4
Roman27011986 <>
2020-10-17 15:03:37 +0300

    dfd

--- src/js/plagin.js
+++ src/js/plagin.js
@@ -1,11 +1,16 @@
-const btnOpen = document.querySelector('.btnOpen');
-const btnClose = document.querySelector('.btnClose');
-const header__menu = document.querySelector('.header__menu');
-const header__btn = document.querySelector('.header__btn');
-const nav__item = document.querySelector('nav__item');
-const tl = new TimelineMax({ paused: true });
-if (document.documentElement.clientWidth < 1200) {
+function getWindowWidth() {
+  return window.innerWidth || document.body.clientWidth;
+}
 
+
+if (getWindowWidth() <= 1200) {
+  const btnOpen = document.querySelector('.btnOpen');
+  const btnClose = document.querySelector('.btnClose');
+  const header__menu = document.querySelector('.header__menu');
+  const header__btn = document.querySelector('.header__btn');
+  const nav__item = document.querySelector('nav__item');
+
+  const tl = new TimelineMax({ paused: true });
 
   tl.timeScale(1);
   tl.to('h6', 0.3, { opacity: 0 })
@@ -24,20 +29,22 @@ if (document.documentElement.clientWidth < 1200) {
       rotation: 360,
       ease: Power1.easeInOut,
     }, '-=0.5')
-    .to(".header__btn", 0.5, {
+    .to(header__btn, 0.9, {
       x: 0,
-     
-      ease: Power2.easeInOut,
-    }, '-=0.18')
+      opacity: 1,
+      rotation: 360,
+      ease: Power1.easeInOut,
+    }, '-=0.5')
     .staggerFrom('.nav__item', 0.2, {
       x: 250,
       ease: Back.easeOut,
     }, 0.06, '-=0.18');
-}
+      
+
+  
+
   openMenu = () => tl.play();
   closeMenu = () => tl.reverse();
   btnOpen.addEventListener('click', openMenu, false);
   btnClose.addEventListener('click', closeMenu, false);
-
-
-  
+}
