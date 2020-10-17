f1bdb2cd79a4e98c4c4ad3b6797660bdd4dcc322
Roman27011986 <>
2020-10-17 15:40:00 +0300

    ывыв

--- src/js/plagin.js
+++ src/js/plagin.js
@@ -1,50 +1,50 @@
-function getWindowWidth() {
-  return window.innerWidth || document.body.clientWidth;
-}
 
 
-if (getWindowWidth() <= 1200) {
   const btnOpen = document.querySelector('.btnOpen');
   const btnClose = document.querySelector('.btnClose');
   const header__menu = document.querySelector('.header__menu');
   const header__btn = document.querySelector('.header__btn');
   const nav__item = document.querySelector('nav__item');
 
-  const tl = new TimelineMax({ paused: true });
-
-  tl.timeScale(1);
-  tl.to('h6', 0.3, { opacity: 0 })
-    .to(btnOpen, 0.7, {
-      x: -300,
-      opacity: 0,
-      ease: Power2.easeInOut,
-    }, '-=0.5')
-    .to(header__menu, 0.5, {
-      x: 0,
-      ease: Power2.easeInOut,
-    }, '-=0.5')
-    .to(btnClose, 0.7, {
-      x: 0,
-      opacity: 1,
-      rotation: 360,
-      ease: Power1.easeInOut,
-    }, '-=0.5')
-    .to(header__btn, 0.9, {
-      x: 0,
-      opacity: 1,
-      rotation: 360,
-      ease: Power1.easeInOut,
-    }, '-=0.5')
-    .staggerFrom('.nav__item', 0.2, {
-      x: 250,
-      ease: Back.easeOut,
-    }, 0.06, '-=0.18');
+const tl = new TimelineMax({ paused: true });
+ 
+ 
+     tl.timeScale(1);
+     tl.to('h6', 0.3, { opacity: 0 })
+       .to(btnOpen, 0.7, {
+         x: -300,
+         opacity: 0,
+         ease: Power2.easeInOut,
+       }, '-=0.5')
+       .to(header__menu, 0.5, {
+         x: 0,
+         ease: Power2.easeInOut,
+       }, '-=0.5')
+       .to(btnClose, 0.7, {
+         x: 0,
+         opacity: 1,
+         rotation: 360,
+         ease: Power1.easeInOut,
+       }, '-=0.5')
+       .to(header__btn, 0.7, {
+         x: 0,
+         opacity: 1,
+         ease: Power2.easeInOut,
+       }, '-=0.9')
+    
+       .staggerFrom('.nav__item', 0.2, {
+         x: 250,
+         ease: Back.easeOut,
+       }, 0.06, '-=0.18');
       
 
+ 
+    
+    openMenu = () => tl.play();
+    closeMenu = () => tl.reverse();
+    btnOpen.addEventListener('click', openMenu, false);
+    btnClose.addEventListener('click', closeMenu, false);
   
 
-  openMenu = () => tl.play();
-  closeMenu = () => tl.reverse();
-  btnOpen.addEventListener('click', openMenu, false);
-  btnClose.addEventListener('click', closeMenu, false);
-}
+	
+	
